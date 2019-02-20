c3d5a975493308de8563c817cd51cc355a028cdd
gwest <gwest@redhat.com>
2019-02-20 14:16:29 -0800

    Add IPV6 disable and general Regset and query

--- app/testit.go
+++ app/testit.go
@@ -6,6 +6,7 @@ import(
 			"strings"
 	)
 
+
 func main(){
   thever := GetWinVersion();
 	fmt.Printf("Windows Version = %s\n",thever);
@@ -13,16 +14,55 @@ func main(){
 }
 
 func check_windows_prereq(){
-		check_ipv6();
+		reset_needed := false;
+		result := check_ipv6();
+		if (result == true){
+			 reset_needed = true;
+		   }
+		if (reset_needed == true){
+ 			reset_needed = false;
+ 			PsReset();
+ 			}
 }
 
-func check_ipv6(){
-	regNamespace := "HKLM:\\SYSTEM\\CurrentControlSet\\Services\\Tcpip6\\Parameters";
-  cmd := "Get-ItemProperty -Path " + regNamespace + " -Name 'DisabledComponents' -ErrorAction SilentlyContinue";
+func PsReset(){
+	fmt.Printf("Reseting\n");
+}
+
+func check_ipv6() bool {
+	thenamespace := "HKLM:\\SYSTEM\\CurrentControlSet\\Services\\Tcpip6\\Parameters";
+	thevalue := GetPsRegValue(thenamespace,"DisabledComponents");
+	if (thevalue == ""){ // Its not disabled}
+	    // Lets Disable it
+			fmt.Printf("Disable IPV6\n");
+			SetPSRegValue(thenamespace,"DisabledComponents","0xffffffff","DWord");
+      return(true);
+		}
+	return(false);
+}
+
+func GetLineArray(theresult string ,thelinenum int ) [] string {
+	thelines := lines(theresult);
+	output := standardizeSpaces(thelines[thelinenum]);
+	varvals := strings.Split(output," ");
+	return(varvals);
+}
+
+func GetPsRegValue(thenamespace string,thevaluename string) string {
+	cmd := "Get-ItemProperty -Path " + thenamespace + " -Name '" + thevaluename + "' -ErrorAction SilentlyContinue";
 	result := powershell(cmd);
-	fmt.Printf("ipv6: %s->\n%s\n",cmd,result)
+	if (len(result) == 0){
+		return "";
+	  }
+	varvals := GetLineArray(result,2);
+	return(varvals[2]);
 }
 
+func SetPSRegValue(thenamespace string, thevaluename string, thevalue string, thetype string){
+	thecmd := "New-ItemProperty -Path " + thenamespace + " -Name '" + thevaluename + "' -Value '" + thevalue + "' -PropertyType '" + thetype + "'";
+  result := powershell(thecmd);
+	fmt.Printf("SetPSRegValue: %s\n", result);
+}
 func GetWinVersion() string {
 // Major  Minor  Build  Revision
 // -----  -----  -----  --------
@@ -47,7 +87,8 @@ func standardizeSpaces(s string) string {
 }
 
 func powershell(thecmd string) string {
-	c,err := exec.Command("powershell", thecmd).CombinedOutput();
+	theargs := strings.Split(thecmd," ");
+	c,err := exec.Command("powershell", theargs...).CombinedOutput();
 	cmd := string(c);
 
 	if  err != nil {
