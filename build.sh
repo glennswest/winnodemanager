3b604d046586c8fa287078d9ea1625268922ec6a
glennswest <gwest@redhat.com>
2019-05-22 14:55:01 -0500

    Cleanup build

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@ export GIT_COMMIT=$(git rev-parse --short HEAD)
 echo $GIT_COMMIT > winnodeman.version
 go get ./...
 GOOS=windows GOARCH=386 go build -o "winnodeman.exe" -ldflags "-X main.builddate=`date -u +.%Y%m%d.%H%M%S` -X main.gitversion=$GIT_COMMIT"  ./winnodeman
-cp winnodeman.exe ../wcontent
-cp winnodeman.version ../wcontent
+cp winnodeman.exe ../wcontent/content
+cp winnodeman.version ../wcontent/content
+git log -1 --pretty="%cd %h %ae %s" >> ../wcontent/updates.md
 (cd ../wcontent;git add winnodeman.exe;git commit -a -m "Winnodeman.exe version updated";git push origin master)
 
 
