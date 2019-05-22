480d7ca2c890a9129400fbe9feabcd4e559289a4
glennswest <gwest@redhat.com>
2019-05-22 14:57:41 -0500

    Fix build notes

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ GOOS=windows GOARCH=386 go build -o "winnodeman.exe" -ldflags "-X main.builddate
 cp winnodeman.exe ../wcontent/content
 cp winnodeman.version ../wcontent/content
 git log -1 --pretty="%cd %h %ae %s" >> ../wcontent/updates.md
-(cd ../wcontent;git add winnodeman.exe;git commit -a -m "Winnodeman.exe version updated";git push origin master)
+(cd ../wcontent;git add content/winnodeman.exe;git add content/winnodeman.version;git commit -a -m "Winnodeman.exe version updated";git push origin master)
 
 
