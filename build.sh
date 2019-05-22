6b144b3d096169036990ac901ef000451cad9245
glennswest <gwest@redhat.com>
2019-05-22 15:02:47 -0500

    Make build commit messages better

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ go get ./...
 GOOS=windows GOARCH=386 go build -o "winnodeman.exe" -ldflags "-X main.builddate=`date -u +.%Y%m%d.%H%M%S` -X main.gitversion=$GIT_COMMIT"  ./winnodeman
 cp winnodeman.exe ../wcontent/content
 cp winnodeman.version ../wcontent/content
-git log -1 --pretty="%cd %h %ae %s" >> ../wcontent/updates.md
-(cd ../wcontent;git add content/winnodeman.exe;git add content/winnodeman.version;git commit -a -m "Winnodeman.exe version updated";git push origin master)
+export GITREASON=`git log -1 --pretty="winnodeman: %cd %h %ae %s"`
+echo ${GITREASON} >> ../wcontent/updates.md
+(cd ../wcontent;git add content/winnodeman.exe;git add content/winnodeman.version;git commit -a -m ${GITREASON};git push origin master)
 
 
