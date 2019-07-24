843f71ffa8fd06290c00ba31016fb886f0370977
gwest <gwest@redhat.com>
2019-07-24 08:41:50 +0800

    Fixup build

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 export GIT_COMMIT=$(git rev-parse --short HEAD)
 echo $GIT_COMMIT > winnodeman.version
-export wcontent=$GOPATH/src/github.com/glennswest/winoperatordata
 go get ./...
-GOOS=windows GOARCH=386 go build -o "winnodeman.exe" -ldflags "-X main.builddate=`date -u +.%Y%m%d.%H%M%S` -X main.gitversion=$GIT_COMMIT"  ./winnodeman
-cp winnodeman.exe ../${wcontent}/wcontent/content
-cp winnodeman.version ../${wcontent}/wcontent/content
+GOOS=windows GOARCH=386 go build -o "winnodeman.exe" -ldflags "-X main.builddate=`date -u +.%Y%m%d.%H%M%S` -X main.gitversion=$GIT_COMMIT" 
+cp winnodeman.exe ../winoperatordata/wcontent/content
+cp winnodeman.version ../winoperatordatawcontent/content
 export GITREASON=`git log -1 --pretty="winnodeman: %cd %h %ae %s"`
-echo ${GITREASON} >> ../${wcontent}/updates.md
-(cd ../${wcontent};git add -f wcontent/content/winnodeman.exe;git add -f wcontent/content/winnodeman.version;git commit -a -m "Update winnodeman executable";git push origin master)
+echo ${GITREASON} >> ../winoperatordata/updates.md
+(cd ../winoperatordata;git add -f wcontent/content/winnodeman.exe;git add -f wcontent/content/winnodeman.version;git add updates.md;git commit -a -m "Update winnodeman executable";git push origin master)
 
 
