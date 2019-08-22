db009993e72def8949017556ec97afc10d15e5dc
glennswest <gwest@redhat.com>
2019-08-22 09:10:33 -0500

    Make sure we wait for cmd execution

--- winnodeman/main.go
+++ winnodeman/main.go
@@ -514,7 +514,7 @@ func process_local_commands(cmds []gjson.Result,nodename string,d string,cname s
           }
      thepath := "/bin/run_" + cname + ".ps1"
      WriteFile(thepath,pshellcmd)
-     cmd := thepath + " 2>&1 > " + "/k/logs/run_" + cname + ".out"
+     cmd := thepath + " *> " + "/k/logs/run_" + cname + ".out | Out-Null"
      pshell.Powershell(cmd)
 }
 
